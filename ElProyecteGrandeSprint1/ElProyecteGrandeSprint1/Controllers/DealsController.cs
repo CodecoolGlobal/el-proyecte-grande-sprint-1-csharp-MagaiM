65f67590423e0c6dc0be3ca92955ec14a4f2ea8e
MagaiM <magai.marton@gmail.com>
2022-05-24 11:41:51 +0200

    Add Deals page

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/DealsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using ElProyecteGrandeSprint1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrandeSprint1.Controllers
+{
+    public class DealsController : Controller
+    {
+        private readonly ILogger<DealsController> _logger;
+
+        public DealsController(ILogger<DealsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
