a1bd65830ded8162c16f3a26be41e942a05e484d
MagaiM <magai.marton@gmail.com>
2022-05-24 11:41:36 +0200

    Add News page

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/NewsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using ElProyecteGrandeSprint1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElProyecteGrandeSprint1.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ILogger<NewsController> _logger;
+
+        public NewsController(ILogger<NewsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
