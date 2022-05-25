6399990cebebab265ace520b717b5f37ba8fe63a
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-05-25 13:09:30 +0200

    added new js for Home / index

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/wwwroot/js/Home.js
@@ -0,0 +1,36 @@
+﻿document.querySelector("body > header > nav > div > a").innerHTML = "nope";
+function fetchResource() {
+    window.fetch(`Home/GetApi?page=1`).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            for (var element in data) {
+                console.log(element);
+                let jumbotronBody = jumbotronBodyBuilder(data[element].title, data[element].user, data[element].time_ago, data[element].url);
+                document.querySelector("#containerbody").insertAdjacentHTML('beforeend', jumbotronBody);
+            }
+        });
+    });
+}
+
+function jumbotronBodyBuilder(Title, Author, TimeAgo, url) {
+    if (Author == null)
+        Author = "Unknown";
+    let jumbotronBody = `
+        <div class="jumbotron">
+            <div>
+                Title : <a href="${url}">${Title}</a>
+                </div>
+            <div>
+                Author : ${Author}
+                </div>
+            <div>
+                TimeAgo : ${TimeAgo}
+                </div>
+        </div>
+    `;
+    return jumbotronBody;
+}
+
+function populateMainMenu() {
+    fetchResource();
+} populateMainMenu();
