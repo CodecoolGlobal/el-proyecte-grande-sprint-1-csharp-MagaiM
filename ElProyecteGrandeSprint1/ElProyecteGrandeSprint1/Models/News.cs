1290c40f5caa35cc3c30be26fc43ea166fbbf771
MagaiM <magai.marton@gmail.com>
2022-05-25 12:00:36 +0200

    Add News modell class

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Models/News.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace ElProyecteGrandeSprint1.Models
+{
+    public class News
+    {
+        //[JsonProperty("title")]
+        public string Title { get; set; }
+
+        //[JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        //[JsonProperty("description")]
+        public string Description { get; set; }
+
+        //[JsonProperty("image")]
+        public string Image { get; set; }
+
+        //[JsonProperty("link")]
+        public string Link { get; set; }
+    }
+}
