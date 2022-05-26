a05aef92965826b4a3b3ea09250001f6a80ac7ac
MagaiM <magai.marton@gmail.com>
2022-05-26 12:55:05 +0200

    Add GNews class for gNews api

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Models/GNews.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace ElProyecteGrandeSprint1.Models
+{
+    public class GNews
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        [JsonProperty("url")]
+        public string Link { get; set; }
+
+        public string Image { get; set; }
+
+        [JsonProperty("publishedAt")]
+        public DateTime Date { get; set; }
+
+        public Source Source { get; set; }
+
+    }
+}
