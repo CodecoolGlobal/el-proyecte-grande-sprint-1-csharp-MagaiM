9f32cb391cb4ae230c336158719c39ea5d416209
Kristóf <bkristof54@gmail.com>
2022-06-08 11:33:29 +0200

    Changed fetch url

--- ElProyecteGrandeSprint1/elproyectegrandefrontend/src/App.js
+++ ElProyecteGrandeSprint1/elproyectegrandefrontend/src/App.js
@@ -52,7 +52,7 @@ export default class App extends Component {
     }
 
     async populateWeatherData() {
-        const response = await fetch('https://localhost:7064/Home/GetApi?pageSize=20');
+        const response = await fetch('https://localhost:7064/Deals/60');
         const data = await response.json();
         this.setState({ forecasts: data, loading: false });
     }
