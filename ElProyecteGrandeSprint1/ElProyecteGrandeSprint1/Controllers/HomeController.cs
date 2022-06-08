1835de966854d9166c521713d5b6b0dfacf5e942
Kristóf <bkristof54@gmail.com>
2022-06-08 11:45:04 +0200

    Refactor HomeController

--- ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/HomeController.cs
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace ElProyecteGrandeSprint1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApiController _apiController = new ApiController();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -26,52 +27,17 @@ namespace ElProyecteGrandeSprint1.Controllers
 
         public async Task<string> GetApi(int pageSize)
         {
-            string apiUrl = $"https://www.cheapshark.com/api/1.0/deals?pageSize={pageSize}";
-
-            using (HttpClient client = new HttpClient())
+            var request = new HttpRequestMessage
             {
-                client.BaseAddress = new Uri(apiUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                if (response.IsSuccessStatusCode)
-                {
-                    var data = response.Content.ReadAsStringAsync().Result;
-                    List<Deal> deserialisation = JsonConvert.DeserializeObject<List<Deal>>(data);
-                    List<Deal> newListDeal = new List<Deal>();
-                    foreach (var element in deserialisation)
-                    {
-                        newListDeal.Add(new Deal
-                        {
-                            StoreName = ((Stores)Int32.Parse(element.StoreName)).ToString(),
-                            Title = element.Title,
-                            SalePrice = element.SalePrice,
-                            NormalPrice = element.NormalPrice,
-                            IsOnSale = element.IsOnSale,
-                            MetacriticScore = element.MetacriticScore,
-                            SteamRatingCount = element.SteamRatingCount,
-                            SteamRatingPercent  = element.SteamRatingPercent,
-                            SteamRatingText = element.SteamRatingText,
-                            DealRating = element.DealRating,
-                            Image = element.Image,
-
-                        });
-                    }
-                    string newdata = System.Text.Json.JsonSerializer.Serialize<List<Deal>>(newListDeal);
-                    return newdata;
-
-                }
-
-
-            }
-
-            return null;
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://www.cheapshark.com/api/1.0/deals?pageSize={pageSize}")
+            };
+            var deals = await _apiController.GetDeals(request);
+            return deals;
         }
 
         public async Task<string> RecentFreeToPlayGames()
         {
-            var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
@@ -82,16 +48,7 @@ namespace ElProyecteGrandeSprint1.Controllers
                     { "X-RapidAPI-Key", "44c0067568mshcd96a9adc6db887p13ebcfjsnc1d0ab0bf882" },
                 },
             };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                List<FreeGame> deserialisation = JsonConvert.DeserializeObject<List<FreeGame>>(body);
-                string newdata = System.Text.Json.JsonSerializer.Serialize<List<FreeGame>>(deserialisation);
-                return newdata;
-            }
-
-            return null;
+            return _apiController.GetDataFromApi<FreeGame>(request);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
