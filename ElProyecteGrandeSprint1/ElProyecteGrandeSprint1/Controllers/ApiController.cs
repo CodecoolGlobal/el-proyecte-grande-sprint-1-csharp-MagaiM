4f12d6b16afd97ff4df99446600dee3289b4fa35
Kristóf <bkristof54@gmail.com>
2022-06-08 11:35:50 +0200

    Add ApiController's methods

--- ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/ApiController.cs
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/ApiController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElProyecteGrandeSprint1.Helpers;
+using ElProyecteGrandeSprint1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ElProyecteGrandeSprint1.Controllers
 {
+
     public class ApiController : Controller
     {
-        public IActionResult GetTopTenNews()
+        private readonly ServiceHelper _serviceHelper = new ServiceHelper();
+        public string GetDataFromApi<T>(HttpRequestMessage request)
         {
-            return null;
+            var body = _serviceHelper.GetJsonStringFromApi(request);
+            string newData;
+            try
+            {
+                var deserializedData = _serviceHelper.DeserializeData<T>(body.Result);
+                newData = _serviceHelper.SerializeData(deserializedData);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return newData;
+            
+        }
+
+        public async Task<string> GetDeals(HttpRequestMessage request)
+        {
+            var body = await _serviceHelper.GetJsonStringFromApi(request);
+            var deserializedData = _serviceHelper.DeserializeData<Deal>(body);
+            var newListDeal = _serviceHelper.MakeDealsObject(deserializedData);
+            var newData = _serviceHelper.SerializeData(newListDeal ?? throw new NullReferenceException());
+            return newData;
         }
     }
 }
