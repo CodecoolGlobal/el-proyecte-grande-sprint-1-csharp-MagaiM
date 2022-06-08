27cae41953dfb0298d1ce515b69a58c6d56434f5
MagaiM <magai.marton@gmail.com>
2022-06-08 18:24:08 +0200

    Add Header.js

---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = ({loadNews}) => {
+  return (
+        <header>
+            <nav className="navbar-dark navbar navbar-expand-sm navbar-toggleable-sm bg-black border-bottom box-shadow mb-3">
+                <div className="container-fluid">
+                    
+                    <a className="navbar-brand"><img className="" src="Imgs/KVMResized.jpg" style={{width: '100px', height: '40px'}}/></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-dark navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                        <ul className="navbar-nav flex-grow-1">
+                            <li className="nav-item">
+                                <a className="nav-link text-light" type='button' onClick={loadNews}>News</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-light">Deals</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+  )
+}
+
+
+Header.prototype = {
+    loadNews: PropTypes.func
+}
+
+export default Header
