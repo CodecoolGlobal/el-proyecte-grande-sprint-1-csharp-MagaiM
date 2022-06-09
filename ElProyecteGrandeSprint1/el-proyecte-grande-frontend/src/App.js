771fece5dc478ac7e0c736c26df964933fcb4190
MagaiM <magai.marton@gmail.com>
2022-06-09 13:36:55 +0200

    Add showGameNews method, Modify loadNews method

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/App.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect, useCallback } from 'react';
 import { useState } from "react";
 import Container from './components/Container';
 import Header from './components/Header';
@@ -6,23 +6,29 @@ import News from './components/News';
 
 function App(){
 
-    const [text , setText] = useState("")
+    const [text , setText] = useState("");
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetchData('https://localhost:7064/News')
-        .then(data => {
-            setData(data);
+        fetchData('https://localhost:7064/News/wtf').then(fetchedData => {
+            setData(fetchedData)
         })
     }, [])
-   
-    const loadNews = () => {
-        setText(<News fetchData={fetchData} showGameNews={showGameNews} />)
-    }
 
-    const showGameNews = (event) => {
-        console.log("ShowGameNewsEvent");
-    };
+    const loadNews = useCallback( () => {
+        fetchData(`https://localhost:7064/News/`)
+            .then(data => {
+            setText(<News fetchData={data} showGameNews={showGameNews} showLatestNews={loadNews} searchedNews={""} />)
+        })
+    }, [])
+
+    const showGameNews = useCallback((event) => {
+        const searchedNews = event.target.textContent;
+        fetchData(`https://localhost:7064/News/${searchedNews}`)
+            .then(data => {
+            setText(<News fetchData={data} showGameNews={showGameNews} showLatestNews={loadNews} searchedNews={searchedNews} />)
+        })
+    }, [])
 
     return (
         <div className='bg-dark text-white'>
@@ -32,13 +38,13 @@ function App(){
     );
 }
 
-    async function fetchData(url) {
-        const response = await fetch(url);
-        if (response.ok){
-            const data = await response.json();
-            return data;
-        }
-        throw response;
+async function fetchData(url) {
+    const response = await fetch(url);
+    if (response.ok){
+        const data = await response.json();
+        return data;
     }
+    throw response;
+}
 
 export default App;
