5b27459c7fb43b5f2a85be1a99d327f5f67a19db
MagaiM <magai.marton@gmail.com>
2022-06-09 18:16:25 +0200

    Update data keys to match updated api keys

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/RecentNewsCard.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/RecentNewsCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const RecentNewsCard = ({news}) => {
-  
+const RecentNewsCard = ({news, formattedDate}) => {
   return (
     <div className="news-card">
-        <img className="card-image" src={String(news.image)}/>
+        <img className="card-image" src={String(news.Image)}/>
         <div className="card-body inner-card">
-            <a className='card-title' href={String(news.link)}><h5 className="card-title">{news.title}</h5></a>
-            <p className="card-text">{news.description}</p>
-            <p className="card-text">{news.date}</p>
+            <a className='card-title' href={String(news.link)}><h5 className="card-title">{news.Title}</h5></a>
+            <p className="card-text">{news.Description}</p>
+            <p className="card-text">{formattedDate}</p>
         </div>
     </div>
   )
