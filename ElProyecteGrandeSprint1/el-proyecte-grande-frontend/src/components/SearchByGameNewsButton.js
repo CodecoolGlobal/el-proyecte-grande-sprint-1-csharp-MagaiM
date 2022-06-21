6bf10a02ab87758b2e5313f6f38d5100ad1b11a9
MagaiM <magai.marton@gmail.com>
2022-06-21 18:17:27 +0200

    Fix OtherNews page reloading issues.

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/SearchByGameNewsButton.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/SearchByGameNewsButton.js
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
 
-const SearchByGameNewsButton = ({button}) => {
-  
+const SearchByGameNewsButton = ({button, index}) => {
+
   return (
     <>
-        <button className="search-by-game-news-button btn-outline-light" type="button">
+        <button className="search-by-game-news-button btn-outline-light" type="button" key={index}>
           <Link className="nav-link text-light" type='button' to={`other-news?search=${button.title}`}>
             <h5>{button.title}</h5>
           </Link>
