c5acde48d65aabbf3a12edc63cb44d616d08688d
MagaiM <magai.marton@gmail.com>
2022-06-21 14:01:06 +0200

    Add News rout jsx

---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/News.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import OtherNewsButtons from '../components/OtherNewsButtons';
+import { Outlet } from "react-router-dom";
+
+const News = () => {
+
+    return (
+    <div className="container">
+        <div className='news-page-container'>
+            <div className="news-container">
+                <Outlet />
+            </div>
+            <OtherNewsButtons />
+        </div>
+    </div>
+  )
+}
+
+export default News
