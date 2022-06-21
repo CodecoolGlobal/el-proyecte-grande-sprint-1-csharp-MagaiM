dd648d3709b62b116eb1924f563b5258216f7e9d
MagaiM <magai.marton@gmail.com>
2022-06-21 14:02:45 +0200

    Add routs to index.js

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/index.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/index.js
@@ -7,11 +7,33 @@ import './Design/Imgs/KVM.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './routes/Home';
+import News from './routes/News';
+import Deals from './routes/Deals';
+import RecentNews from './routes/RecentNews';
+import OtherNews from './routes/OtherNews';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />}>
+            <Route path='' element={<RecentNews />} />
+            <Route path='/news/other-news' element={<OtherNews />} />
+          </Route>
+          <Route path="deals" element={<Deals />} />
+          <Route path="*" element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
