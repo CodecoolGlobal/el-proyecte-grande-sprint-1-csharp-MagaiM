bfe2633bb722476845e4900eb44f92997ae6b16c
Kristóf <bkristof54@gmail.com>
2022-06-22 18:41:48 +0200

    Added lists of objects for checkboxes

---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/utils/PageSize.js
@@ -0,0 +1,18 @@
+export const pageSize=[
+    {
+        setData: 60,
+        Title: "60"
+    },    {
+        setData: 50,
+        Title: "50"
+    },    {
+        setData: 40,
+        Title: "40"
+    },    {
+        setData: 30,
+        Title: "30"
+    },    {
+        setData: 20,
+        Title: "20"
+    },
+]
---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/utils/StoreNames.js
@@ -0,0 +1,86 @@
+export const storeNames=[
+    {
+        setData: "",
+        Title: "All"
+    },
+    {
+        setData: 1,
+        Title: "Steam"
+    },
+    {
+        setData: 2,
+        Title: "GamersGate"
+    },
+    {
+        setData: 3,
+        Title: "GreenManGaming"
+    },
+    {
+        setData: 6,
+        Title: "Direct2Drive"
+    },
+    {
+        setData: 7,
+        Title: "GOG"
+    },
+    {
+        setData: 8,
+        Title: "Origin"
+    },
+    {
+        setData: 11,
+        Title: "Humble Store"
+    },
+    {
+        setData: 13,
+        Title: "Uplay"
+    },
+    {
+        setData: 15,
+        Title: "Fanatical"
+    },
+    {
+        setData: 21,
+        Title: "WinGameStore"
+    },
+    {
+        setData: 23,
+        Title: "GameBillet"
+    },
+    {
+        setData: 24,
+        Title: "Voidu"
+    },
+    {
+        setData: 25,
+        Title: "Epic Games Store"
+    },
+    {
+        setData: 27,
+        Title: "Gamesplanet"
+    },
+    {
+        setData: 28,
+        Title: "Gamesload"
+    },
+    {
+        setData: 29,
+        Title: "2Game"
+    },
+    {
+        setData: 30,
+        Title: "IndieGala"
+    },
+    {
+        setData: 31,
+        Title: "Blizzard Shop"
+    },
+    {
+        setData: 32,
+        Title: "AllYouPlay"
+    },
+    {
+        setData: 33,
+        Title: "DLGamer"
+    },
+]
