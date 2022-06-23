59d3c34826d5f103c50e887ba05feaff75d3f754
MagaiM <magai.marton@gmail.com>
2022-06-23 13:01:27 +0200

    Add set cookie on login

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/Login.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/Login.js
@@ -1,31 +1,29 @@
 import PropTypes from "prop-types";
 import UserForm from "../components/UserForm";
-import { useAppContext } from "../lib/contextLib";
-
+import { useCookies } from 'react-cookie';
 
 const Login = ({postData, getData}) => {
 
+  let User;
+  const [cookies, setCookie] = useCookies(['user_id']);
 
-const { userHasAuthenticated } = useAppContext();
-let User;
   const SendDataToBackEnd = async (inputs) => {
       let validateResponse = await postData("Validate", inputs);
       console.log(validateResponse);
       if (validateResponse === "True") {
           User = await getData(`name/${inputs["username"]}`)
-          userHasAuthenticated(true);
-          console.log(User);
+          setCookie('user_id', User["id"], { path: '/' });
       }
-  }
+    }
 
   return (
     <UserForm postData={postData} SendDataToBackEnd={SendDataToBackEnd}></UserForm>
   )
 }
+
 Login.prototype = {
     postData: PropTypes.func,
     getData: PropTypes.func
 }
-
 
 export default Login
