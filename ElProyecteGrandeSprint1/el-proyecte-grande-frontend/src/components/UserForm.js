9e7ad584be3e59d77736ba586278af54f0e06384
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-06-23 09:53:49 +0200

    logen / registre form

---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/UserForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+import PropTypes from "prop-types";
+
+const UserForm = ({SendDataToBackEnd}) => {
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const UserName = event.target.name;
+    const Password = event.target.value;
+    setInputs(values => ({...values, [UserName]: Password}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+    SendDataToBackEnd(inputs);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input
+        type="text"
+        name="username"
+        value={inputs.username || ""}
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your age:
+        <input
+          type="text"
+          name="password"
+          value={inputs.password || ""}
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
+}
+UserForm.prototype = {
+    postData: PropTypes.func,
+    SendDataToBackEnd: PropTypes.func
+}
+
+
+export default UserForm
