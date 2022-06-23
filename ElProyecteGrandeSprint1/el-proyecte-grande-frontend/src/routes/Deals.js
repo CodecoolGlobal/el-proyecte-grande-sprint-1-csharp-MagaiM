daa5e4f87fea8ee960d8cf81807637e322f45772
Kristóf <bkristof54@gmail.com>
2022-06-23 13:07:56 +0200

    Refactored deals page to use Checkboxes component

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/Deals.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/Deals.js
@@ -7,10 +7,12 @@ import DealsTable from '../components/DealsTable';
 import {sortByList} from '../utils/Sortby';
 import { filterDirectionList } from '../utils/FilterDirection';
 import { storeNames } from '../utils/StoreNames';
+import Checkboxes from '../components/Checkboxes';
+
 
 const Deals = () => {
     const [dealsData, setDealsData] = useState([]);
-    const [isChecked, setIsChecked] = useState(new Array(sortByList.length+filterDirectionList.length).fill(false));
+    const [isChecked, setIsChecked] = useState(new Array(sortByList.length+filterDirectionList.length+storeNames.length).fill(false));
     const [baseSortBy, setBaseSortBy] = useState("Deal Rating");
     const [sortBy, setSortBy] = useState("Deal Rating");
     const [basePageSize, setBasePageSize] = useState(60);
@@ -19,8 +21,9 @@ const Deals = () => {
     const [filterDirection, setFilterDirection] = useState(0);
     const [baseStoreId, setBaseStoreId] = useState("")
     const [storeId, setStoreId] = useState("")
-    const [lastUrl, setLastUrl] = useState(`https://localhost:44321/Deals?sortBy=${sortBy}&pageSize=${pageSize}&desc=${filterDirection}&storeId=${storeId}`)
     const [Url, setUrl] = useState(`https://localhost:44321/Deals?sortBy=${sortBy}&pageSize=${pageSize}&desc=${filterDirection}&storeId=${storeId}`)
+
+
     useEffect(() => {
       fetchData(Url)
       .then(dealsData => {
@@ -28,23 +31,16 @@ const Deals = () => {
       })
   }, [])
 
-    useEffect(() => {
-        fetchData('https://localhost:44321/Deals?sortBy=Deal Rating')
-        .then(dealsData => {
-            setDealsData(dealsData);
-        })
-    }, [])
-
-useEffect(() => {
+  useEffect(() => {
     fetchData(Url)
     .then(dealsData => {
         setDealsData(dealsData);
     })
-}, [Url]);
+  }, [Url]);
 
-useEffect(() => {
+  useEffect(() => {
     setUrl(`https://localhost:44321/Deals?sortBy=${sortBy}&pageSize=${pageSize}&desc=${filterDirection}&storeId=${storeId}`)
-}, [isChecked]);
+  }, [isChecked]);
 
 
   const handleOnChange = (checkboxIndex, value) => {
@@ -59,100 +55,29 @@ useEffect(() => {
     updatedCheckedState.map((item, index)  =>{
       if(checkboxIndex === index && item === false){
         switch(index){
-          case 0:
-          case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-          case 6:
-          case 7:
-          case 8:
+          case 0:case 1:case 2:case 3:case 4:case 5:case 6:case 7:case 8:
             setSortBy(baseSortBy)
             break;
-          case 9:
-          case 10:
+          case 9:case 10:
             setFilterDirection(baseFilterDirection)
             break;
-          case 11:
-          case 12:
-          case 13:
-          case 14:
-          case 15:
-          case 16:
-          case 17:
-          case 18:
-          case 19:
-          case 20:
-          case 21:
-          case 22:
-          case 23:
-          case 24:
-          case 25:
-          case 26:
-          case 27:
-          case 28:
-          case 29:
-          case 30:
-          case 31:
-          case 32:
+          case 11:case 12:case 13:case 14:case 15:case 16:case 17:case 18:case 19:case 20:case 21:
+          case 22:case 23:case 24:case 25:case 26:case 27:case 28:case 29:case 30:case 31:case 32:
             setStoreId(baseStoreId)
             break;
-
         }
       }});
     setIsChecked(updatedCheckedState);
-    
-
     };
+
+
   const dealsPageContent = 
   (
-  
   <div className="deals-page-container">
     <div className='filter-bar'>
-      <ul className='checkbox-div'>
-        <div className='checkbox-div-title'>SortBy</div>
-
-      {sortByList.map(({setData, Title},index)=>(
-         <li className="checkbox" key={index}>
-         <input
-           type="radio"
-           name='sortBy'
-           checked={isChecked[index]}
-           onChange={e => {setSortBy(setData); handleOnChange(index)}}></input>
-         {Title}
-         </li>
-        ))}
-      </ul>
-      <ul className='checkbox-div'>
-      <div className='checkbox-div-title'>Direction</div>
-
-      {filterDirectionList.map(({setData, Title},index)=>(
-        <li className="checkbox" key={sortByList.length+index}>
-        <input
-          type="radio"
-          name='direction'
-          checked={isChecked[sortByList.length+index]}
-          onChange={e => {setFilterDirection(setData); handleOnChange(sortByList.length+index)}}></input>
-        {Title}
-        </li>
-      ))}
-      </ul>
-      <ul className='checkbox-div'>
-      <div className='checkbox-div-title'>Stores</div>
-
-      {storeNames.map(({setData, Title},index)=>(
-        <li className="checkbox" key={sortByList.length+index}>
-        <input
-          type="radio"
-          name='Stores'
-          checked={isChecked[sortByList.length+filterDirectionList.length+index]}
-          onChange={e => {setStoreId(setData); handleOnChange(sortByList.length+filterDirectionList.length+index)}}></input>
-        {Title}
-        </li>
-      ))}
-      </ul>
-      <div className='checkbox-div'></div>
+      <Checkboxes handleOnChange={handleOnChange} set={setSortBy} isChecked={isChecked} List={sortByList} name={"SortBy"}></Checkboxes>
+      <Checkboxes handleOnChange={handleOnChange} set={setFilterDirection} isChecked={isChecked} List={filterDirectionList} name={"Direction"}></Checkboxes>
+      <Checkboxes handleOnChange={handleOnChange} set={setStoreId} isChecked={isChecked} List={storeNames} name={"Stores"}></Checkboxes>
     </div>
     <div className='divider'></div>
     <div className='cards-div'>
@@ -164,7 +89,6 @@ useEffect(() => {
 
 return dealsPageContent;
 }
-
 
 
 async function fetchData(url) {
