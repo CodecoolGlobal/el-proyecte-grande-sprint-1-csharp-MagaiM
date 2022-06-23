70a4e27267f91ce3cb82229ea88c1f6da146dd23
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-06-23 17:50:54 +0200

    added the from style

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/UserForm.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/components/UserForm.js
@@ -1,50 +1,72 @@
 import React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 import PropTypes from "prop-types";
+import InvalidLoginPopUp from "./InvalidLoginPopUp";
 
-const UserForm = ({SendDataToBackEnd}) => {
+const UserForm = ({SendDataToBackEnd, Page}) => {
 
-  const [inputs, setInputs] = useState({});
+    const [inputs, setInputs] = useState({});
 
-  const handleChange = (event) => {
-    const UserName = event.target.name;
-    const Password = event.target.value;
-    setInputs(values => ({...values, [UserName]: Password}))
-  }
+    const handleChange = (event) => {
+        const UserName = event.target.name;
+        const Password = event.target.value;
+        setInputs(values => ({...values, [UserName]: Password}))
+    }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-    SendDataToBackEnd(inputs).then(_ => {
-      window.location.href="/";
-    })
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs);
+        SendDataToBackEnd(inputs).then(data => {
+            console.log(data);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input
-        type="text"
-        name="username"
-        value={inputs.username || ""}
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your password:
-        <input
-          type="text"
-          name="password"
-          value={inputs.password || ""}
-          onChange={handleChange}
-        />
-        </label>
-          <input type="submit" />
-    </form>
-  )
+            if (data === "false"){
+                alert("Your username or password is incorrect")
+            }else if(data === "true" || data === "accepted"){
+                window.location.href = "/";
+            }
+            else{
+                alert(data)
+            }
+        })
+    }
+
+    return (
+        <div style={{height: "100%", width: "100%"}}>
+        <form className={'form-style-1'} onSubmit={handleSubmit}>
+            <h3 style={{ textAlignLast: "center", padding: "1rem" }} >{Page}</h3>
+            <label>Enter your name:
+                <br></br>
+                <input
+                    className={"search-by-game-news-button btn-outline-light"}
+                    type="text"
+                    name="username"
+                    value={inputs.username || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <br></br>
+            <label>Enter your password:
+                <br></br>
+                <input
+                    className={"search-by-game-news-button btn-outline-light"}
+                    type="password"
+                    name="password"
+                    value={inputs.password || ""}
+                    onChange={handleChange}
+                />
+                <br></br>
+            </label>
+            <div style={{ textAlignLast: "center", padding: "2rem" }}>
+            <input className={"search-by-game-news-button btn-outline-light"} type="submit"/>
+            </div>
+        </form>
+    </div>
+    )
 }
 UserForm.prototype = {
     postData: PropTypes.func,
-    SendDataToBackEnd: PropTypes.func
+    SendDataToBackEnd: PropTypes.func,
+    Page: PropTypes.string
 }
 
 
