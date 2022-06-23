eb322f2cbd0504b58240f9eaf1a181b14bf1f639
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-06-23 09:54:15 +0200

    created login page

---
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/Login.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import UserForm from "../components/UserForm";
+import { useAppContext } from "../lib/contextLib";
+
+
+const Login = ({postData, getData}) => {
+
+
+const { userHasAuthenticated } = useAppContext();
+let User;
+  const SendDataToBackEnd = async (inputs) => {
+      let validateResponse = await postData("Validate", inputs)
+      if (validateResponse === "True") {
+          User = await getData(`name/${inputs["username"]}`)
+          userHasAuthenticated(true);
+          console.log(User);
+      }
+  }
+
+  return (
+    <UserForm postData={postData} SendDataToBackEnd={SendDataToBackEnd}></UserForm>
+  )
+}
+Login.prototype = {
+    postData: PropTypes.func,
+    getData: PropTypes.func
+}
+
+
+export default Login
