d423bc7af4c9f71fcbe7490350191412848ce6ca
MagaiM <magai.marton@gmail.com>
2022-07-19 20:27:23 +0200

    Add LoginUser.cs
    Co-authored-by: Kristóf <Knurlag@users.noreply.github.com>
    Co-authored-by: Phyxick <Phyxick@users.noreply.github.com>

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Models/Entities/ApiEntities/LoginUser.cs
@@ -0,0 +1,8 @@
+﻿namespace ElProyecteGrandeSprint1.Models.Entities.ApiEntities
+{
+    public class LoginUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
