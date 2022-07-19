f61ee525005ce5ffa7b6d762755ff2029828cff8
MagaiM <magai.marton@gmail.com>
2022-07-19 20:04:16 +0200

    Add roles to database if db is empty
    Co-authored-by: Kristóf <Knurlag@users.noreply.github.com>
    Co-authored-by: Phyxick <Phyxick@users.noreply.github.com>

--- ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/DbInitializer.cs
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/DbInitializer.cs
@@ -10,23 +10,41 @@ namespace ElProyecteGrandeSprint1
         {
             context.Database.EnsureCreated();
 
-            // Look for any students.
-            if (context.Users.Any())
+            // Look for any user.
+            if (!context.Roles.Any())
             {
-                return;   // DB has been seeded
+                var roles = new UserRole[]
+                {
+                    new UserRole {Name = "User"},
+                    new UserRole {Name = "Moderator"},
+                    new UserRole {Name = "Admin"},
+                    new UserRole {Name = "Editor"},
+                };
+
+                foreach (var role in roles)
+                {
+                    context.Roles.Add(role);
+                }
+
+                context.SaveChanges();
             }
+
+            if (context.Users.Any()) return;   // DB has been seeded
 
             var users = new User[]
             {
-            new User{UserName = "Admin", Password = new Password(){UserPassword = "Admin"}, Reputation = 5000, Rank = Rank.Admin},
-            new User{UserName = "Developer", Password = new Password(){UserPassword = "Developer"}, Reputation = 500000, Rank = Rank.Developer},
-            new User{UserName = "Moderator", Password = new Password(){UserPassword = "Moderator"}, Reputation = 5, Rank = Rank.Moderator},
+            new User{UserName = "Admin", Password = new Password(){UserPassword = "Admin"}, Email = "Admin@Admin.Admin", Reputation = 5000, Roles = context.Roles.Where(r => r.Name == "User" || r.Name == "Admin").ToHashSet()},
+            new User{UserName = "Editor", Password = new Password(){UserPassword = "Editor"}, Email = "Editor@Editor.Editor", Reputation = 2500, Roles = context.Roles.Where(r => r.Name == "User" || r.Name == "Editor").ToHashSet()},
+            new User{UserName = "Moderator", Password = new Password(){UserPassword = "Moderator"}, Email = "Moderator@Moderator.Moderator", Reputation = 3000, Roles = context.Roles.Where(r => r.Name == "User" || r.Name == "Moderator").ToHashSet()},
+            new User{UserName = "User", Password = new Password(){UserPassword = "User"}, Email = "User@User.User", Reputation = 1000, Roles = context.Roles.Where(r => r.Name == "User").ToHashSet()},
             };
             foreach (User s in users)
             {
                 context.Users.Add(s);
             }
             context.SaveChanges();
+
+
         }
     }
 }
