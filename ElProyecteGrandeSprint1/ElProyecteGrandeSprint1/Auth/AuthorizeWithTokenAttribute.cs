1f8de27cbbf4147b89eb417d15c364754e60dadc
MagaiM <magai.marton@gmail.com>
2022-07-19 20:09:13 +0200

    Add AuthorizeWithTokenAttribute.cs
    Co-authored-by: Kristóf <Knurlag@users.noreply.github.com>
    Co-authored-by: Phyxick <Phyxick@users.noreply.github.com>

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Auth/AuthorizeWithTokenAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using ElProyecteGrandeSprint1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ElProyecteGrandeSprint1.Auth
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeWithTokenAttribute : Attribute, IAuthorizationFilter
+    {
+        private ApplicationDbContext _applicationDbContext;
+        public AuthorizeWithTokenAttribute(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (token == null || _applicationDbContext.JWTTokens.FirstOrDefault(t => t.Token == token) == null)
+            {
+                // not logged in
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
