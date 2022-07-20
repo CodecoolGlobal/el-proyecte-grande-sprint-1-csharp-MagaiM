35457b8a55c261258315cbcc4f2ea69f67abb166
MagaiM <magai.marton@gmail.com>
2022-07-20 16:40:19 +0200

    Move nav bar to header component, rewrite Routes in App.js

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/App.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/App.js
@@ -10,6 +10,11 @@ import Profile from "./routes/Profile";
 import BoardUser from "./components/BoardUser";
 import BoardModerator from "./components/BoardModerator";
 import BoardAdmin from "./components/BoardAdmin";
+import Header from './components/Header';
+import News from './routes/News';
+import Deals from './routes/Deals';
+import RecentNews from './routes/RecentNews';
+import OtherNews from './routes/OtherNews';
 
 const App = () => {
   const [showModeratorBoard, setShowModeratorBoard] = useState(false);
@@ -23,87 +28,43 @@ const App = () => {
       setShowAdminBoard(user.Roles.includes("Admin"));
     }
   }, []);
-  const logOut = () => {
-    AuthService.logout();
-  };
 
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/"} className="navbar-brand">
-          bezKoder
-        </Link>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/home"} className="nav-link">
-              Home
-            </Link>
-          </li>
-          {showModeratorBoard && (
-            <li className="nav-item">
-              <Link to={"/mod"} className="nav-link">
-                Moderator Board
-              </Link>
-            </li>
-          )}
-          {showAdminBoard && (
-            <li className="nav-item">
-              <Link to={"/admin"} className="nav-link">
-                Admin Board
-              </Link>
-            </li>
-          )}
-          {currentUser && (
-            <li className="nav-item">
-              <Link to={"/user"} className="nav-link">
-                User
-              </Link>
-            </li>
-          )}
-        </div>
-        {currentUser ? (
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/profile"} className="nav-link">
-                {currentUser.username}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a href="/login" className="nav-link" onClick={logOut}>
-                LogOut
-              </a>
-            </li>
-          </div>
-        ) : (
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/register"} className="nav-link">
-                Sign Up
-              </Link>
-            </li>
-          </div>
-        )}
-      </nav>
+    <div className='bg-dark text-white app-container'>
+      <Header currentUser={currentUser} showModeratorBoard={showModeratorBoard} showAdminBoard={showAdminBoard} />
       <div className="container mt-3">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<Home fetchData={fetchData} />} />
+          <Route path="/home" element={<Home fetchData={fetchData} />} />
+          <Route path="/news" element={<News />}>
+            <Route path='' element={<RecentNews fetchData={fetchData}/>}/>
+            <Route path='/news/other-news' element={<OtherNews fetchData={fetchData}/>}/>
+          </Route>
+          <Route path="deals" element={<Deals fetchData={fetchData}/>}/>
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/profile" element={<Profile/>} />
           <Route path="/user" element={<BoardUser/>} />
           <Route path="/mod" element={<BoardModerator/>} />
           <Route path="/admin" element={<BoardAdmin/>} />
+          <Route path="*" element={
+                          <main style={{padding: "1rem"}}>
+                            <p>There's nothing here!</p>
+                          </main>}/>
         </Routes>
       </div>
     </div>
   );
 };
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (response.ok){
+      const data = await response.json();
+      return data;
+  }
+  throw response;
+}
 
 export default App;
 
