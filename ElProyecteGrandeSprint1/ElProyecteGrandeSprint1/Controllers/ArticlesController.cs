1de72bcdc3302a06be4587e124ed2f34c1da77ce
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-07-20 19:56:20 +0200

    Added Article Controller

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/ArticlesController.cs
@@ -0,0 +1,31 @@
+﻿using ElProyecteGrandeSprint1.Auth;
+using ElProyecteGrandeSprint1.Models;
+using ElProyecteGrandeSprint1.Models.Entities.DatabaseEntities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ElProyecteGrandeSprint1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [AuthorizeWithToken("Admin,User")]
+    //[Authorize(Roles = "Admin")]
+    public class ArticlesController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        public ArticlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("/articles")]
+        public Task<List<Article>> GetArticles()
+        {
+            return _context.GetArticles();
+        }
+    }
+}
