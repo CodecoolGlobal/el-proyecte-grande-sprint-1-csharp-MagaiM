23e6ccfbbdf4d9a4d0aa80dfab04e1cd14a74331
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-07-20 19:56:05 +0200

    Added article data entity

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Models/Entities/DatabaseEntities/Article.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElProyecteGrandeSprint1.Models.Entities.DatabaseEntities;
+
+public class Article
+{
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public User Author { get; set; }
+
+    public Theme Theme { get; set; }
+
+    public string ArticleText { get; set; }
+}
