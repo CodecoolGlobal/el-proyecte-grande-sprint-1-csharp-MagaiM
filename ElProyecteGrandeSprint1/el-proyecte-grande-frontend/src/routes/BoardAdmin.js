24663f8a9628e20896ddcac0d4be0fcb835bf705
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-07-21 17:27:37 +0200

    added routes for admin page

--- ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/BoardAdmin.js
+++ ElProyecteGrandeSprint1/el-proyecte-grande-frontend/src/routes/BoardAdmin.js
@@ -1,32 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import UserService from "../services/user.service";
+import ProfileDetailsWithSmallNames from "../components/ProfileDetailsWithSmallNames";
 
-const BoardUser = () => {
-  const [content, setContent] = useState("");
-  useEffect(() => {
-    UserService.getAdminBoard().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-        setContent(_content);
-      }
+const BoardAdmin = () => {
+    const [content, setContent] = useState([]);
+    const [currentProfile, setcurrentProfile] = useState(undefined);
+
+    useEffect(() => {
+        UserService.getAdminBoard().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                setContent(_content);
+            }
+        );
+    }, []);
+
+
+    const openProfile = (e) => {
+        content.forEach(element => {
+            if (element.id == e.target.dataset.id) {
+                setcurrentProfile(element);
+            }
+        });
+    };
+
+
+    const closeProfile = () =>{
+        setcurrentProfile(undefined);
+    }
+
+    if (currentProfile) return (<ProfileDetailsWithSmallNames currentUser={currentProfile} closeProfile={closeProfile}/>);
+    return (
+        <div className="container">
+            <h1><strong style={{color: "rgba(255,255,255,.55)"}}>Users</strong></h1>
+            {content.map((item, index) => {
+                return (
+                    <div key={index} className="other-news-card">
+                        <a className='card-title' onClick={openProfile}><h5 className="card-title" data-id={item.id}>{item.userName}</h5></a>
+                    </div>
+                )
+            })}
+        </div>
     );
-  }, []);
-
-  return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-      </header>
-    </div>
-  );
 };
 
-export default BoardUser;
+export default BoardAdmin;
