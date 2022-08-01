ee40b55782ed2ff3cb658e74419050b7da84cf1f
Kristóf <bkristof54@gmail.com>
2022-08-01 15:41:52 +0200

    Refactor UserController
    Co-authored-by: MagaiM <MagaiM@users.noreply.github.com>

--- ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/UserController.cs
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Controllers/UserController.cs
@@ -2,8 +2,8 @@
 using ElProyecteGrandeSprint1.Models;
 using ElProyecteGrandeSprint1.Models.Entities.ApiEntities;
 using ElProyecteGrandeSprint1.Models.Entities.DatabaseEntities;
+using ElProyecteGrandeSprint1.Services;
 using Microsoft.AspNetCore.Cors;
-
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
@@ -18,76 +18,76 @@ namespace ElProyecteGrandeSprint1.Controllers
     public class UserController : ControllerBase
     {
 
-        private readonly ApplicationDbContext _context;
+        private readonly UserService _userService;
 
 
-        public UserController(ApplicationDbContext context)
+        public UserController(UserService userService)
         {
-            _context = context;
- 
+            _userService = userService;
         }
+
         [HttpGet("{id}")]
         public async Task<User> GetUserData(int id)
         {
-            return await _context.GetUserDataFromDataBase(id);
+            return await _userService.GetUserDataFromDataBase(id);
         }
 
         [HttpGet("/name/{name}")]
         public async Task<User> GetUserData(string name)
         {
-            return await _context.GetUserByName(name);
+            return await _userService.GetUserByName(name);
         }
 
         [HttpPost("/{email}")]
         public async Task<bool> ValidateEmail(string email)
         {
-            return await _context.ValidateEmailForPassword(email);
+            return await _userService.ValidateEmailForPassword(email);
         }
 
         [HttpPost("/login")]
         public async Task<string> Login(LoginUser user)
         {
-            return await _context.Login(user);
+            return await _userService.Login(user);
         }
 
         [HttpPost("/register")]
         public async Task<string> Register([FromBody] RegisterUser user)
         {
-            return await _context.RegisterUser(user);
+            return await _userService.RegisterUser(user);
         }
 
         [HttpPut("{id}")]
         public Task<string> ChangeUserData(int id, [FromBody] RegisterUser user)
         {
-            return _context.ChangeUserProfile(id, user, Guid.Empty);
+            return _userService.ChangeUserProfile(id, user, Guid.Empty);
         }
 
         [HttpDelete("{id}")]
         public Task<string> DeleteUserProfile(int id)
         {
-            return _context.DeleteUser(id);
+            return _userService.DeleteUser(id);
         }
 
         [HttpPost("/send")]
         public void SendEmail([FromBody] RegisterUser user)
         { 
             Guid guid = Guid.NewGuid();
-            _context.SendForgotPasswordEmail(user.Email, guid);
+            _userService.SendForgotPasswordEmail(user.Email, guid);
         }
 
         [HttpPost("/password/{emailId}")]
         public Task<string> ChangePassword(Guid emailId, [FromBody] RegisterUser user)
         {
-            var email =_context.getEmailFromGuid(emailId);
+            var email = _userService.getEmailFromGuid(emailId);
             if (email == null)
             {
                 return Task.FromResult("No");
             }
-            long userId = _context.GetUserByEmail(email.Email).Result.ID;
-            var result = _context.ChangeUserProfile(userId, user, email.Guid);
+            long userId = _userService.GetUserByEmail(email.Email).Result.ID;
+            var result = _userService.ChangeUserProfile(userId, user, email.Guid);
             if (result.Result == "\"Your profile was Changed successfully\"")
             {
-                _context.SendSuccessfulPasswordChangeEmail(emailId);
+                _userService.SendSuccessfulPasswordChangeEmail(emailId);
                 return result;
             }
             return result;
