0a878637c83dcd294b48755372b246b8d3e966b7
Kristóf <bkristof54@gmail.com>
2022-08-01 15:32:43 +0200

    Add ApiService , moved relevant methods from ApplicationDbContext
    Co-authored-by: MagaiM <MagaiM@users.noreply.github.com>

---
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Services/ApiService.cs
@@ -0,0 +1,53 @@
+﻿using ElProyecteGrandeSprint1.Helpers;
+using ElProyecteGrandeSprint1.Models;
+using ElProyecteGrandeSprint1.Models.Entities.ApiEntities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ElProyecteGrandeSprint1.Services
+{
+
+    public class ApiService
+    {
+        private readonly ServiceHelper _serviceHelper = new ServiceHelper();
+
+        public string GetDataFromApi<T>(HttpRequestMessage request)
+        {
+            var body = _serviceHelper.GetJsonStringFromApi(request);
+            string newData;
+
+            try
+            {
+                if (typeof(T) == typeof(GNewsResponse))
+                {
+                    var deserialization = JsonConvert.DeserializeObject<GNewsResponse>(body.Result);
+                    if (deserialization == null) throw new Exception("deserialization was null");
+                    newData = _serviceHelper.SerializeData(deserialization.Articles);
+                }
+                else
+                {
+                    var deserializedData = _serviceHelper.DeserializeData<T>(body.Result);
+                    newData = _serviceHelper.SerializeData(deserializedData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+            return newData;
+        }
+
+        public async Task<string> GetDeals(HttpRequestMessage request)
+        {
+            var body = await _serviceHelper.GetJsonStringFromApi(request);
+            var deserializedData = _serviceHelper.DeserializeData<Deal>(body);
+            if (deserializedData == null) throw new Exception("deserializedData was null");
+            deserializedData = new List<Deal>(deserializedData.DistinctBy(x => x.Title));
+            var newListDeal = _serviceHelper.MakeDealsObject(deserializedData);
+            var newData = _serviceHelper.SerializeData(newListDeal ?? throw new NullReferenceException());
+            return newData;
+        }
+    }
+}
