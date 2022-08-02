e717f228b954bd28fbda8c40cbc5ec458bfc2392
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-08-02 12:55:54 +0200

    Created testcases for ApplicationDbContextHelper

--- ElProyecteGrandeSprint1/TestBackend/UnitTest1.cs
+++ ElProyecteGrandeSprint1/TestBackend/UnitTest1.cs
@@ -1,20 +1,27 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
+using ElProyecteGrandeSprint1.Controllers;
 using ElProyecteGrandeSprint1.Helpers;
 using ElProyecteGrandeSprint1.Models;
+using ElProyecteGrandeSprint1.Models.Entities.ApiEntities;
 using ElProyecteGrandeSprint1.Models.Entities.DatabaseEntities;
 using ElProyecteGrandeSprint1.Models.Enums;
 using ElProyecteGrandeSprint1.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NUnit.Framework;
 
 namespace TestBackend;
 
 public class Tests
 {
+    private byte[] secret = Encoding.ASCII.GetBytes("MY_SECRET_KEY_dasmd.-dDUNJUOFAOD");
+
     private ApplicationDbContext _context;
     private readonly ApplicationDbContextHelper _contextHelper = new();
     private readonly EmailSender _emailSender = new();
-    private DbContextOptions<ApplicationDbContext> _options;
 
     [SetUp]
     public void Setup()
@@ -96,25 +103,74 @@ public class Tests
 
             context.SaveChanges();
         }
+        _context = new ApplicationDbContext(options);
 
-        _options = options;
     }
 
     [Test]
-    public void Test1()
+    public void TestGetUsernameMethodInApplicationDbContext()
     {
-        using (var context = new ApplicationDbContext(_options))
-        {
-            var service = new UserService(context, _contextHelper, _emailSender);
+
+            var service = new UserService(_context, _contextHelper, _emailSender);
             var admin = service.GetUserByName("Admin").Result;
-            var newadmin = context.Users.Include(u => u.Roles).ToListAsync().Result
+            var newadmin = _context.Users.Include(u => u.Roles).ToListAsync().Result
                 .First(x => x.UserName == "Admin");
             Assert.AreSame(newadmin, admin);
-        }
+        
     }
 
     [Test]
-    public void Test2()
+    public void TestApplicationDbContextHelperValidateEmailMethod()
+    {
+        Assert.IsFalse(_contextHelper.ValidateEmail("Moderator@Moderator.Moderator", _context.Users));
+        Assert.IsTrue(_contextHelper.ValidateEmail("Moderator@Moderator.Mderator", _context.Users));
+    }
+
+    [Test]
+    public void TestApplicationDbContextHelperValidateUsernameMethod()
+    {
+        Assert.IsFalse(_contextHelper.ValidateUsername("Moderator", _context.Users));
+        Assert.IsTrue(_contextHelper.ValidateUsername("Moderator@Moderator.Mderator", _context.Users));
+    }
+
+    [TestCase("Nincsen", ExpectedResult = "accepted")]
+    [TestCase("nin", ExpectedResult = "The password must be longer than 5 characters")]
+    [TestCase("nincsen", ExpectedResult = "The password must contain minimum 1 upper letter")]
+    public string TestApplicationDbContextHelperValidatePasswordMethod(string password)
+    {
+        RegisterUser newUserWithCorrectPassword = new RegisterUser
+        {
+            Password = password,
+        };
+        return _contextHelper.ValidatePassword(newUserWithCorrectPassword);
+    }
+    [TestCase("Moderator@Moderator.Moderator","Moderator",3, ExpectedResult = true)]
+    [TestCase("Admin@Admin.Admin", "Admin", 1, ExpectedResult = true)]
+    [TestCase("Editor@Editor.Editor", "Editor", 2, ExpectedResult = true)]
+    public bool TestApplicationDbContextHelperJWTGeneratorMethod(string email, string name, long id)
+    {
+        var generatedJWT = _contextHelper.JWTGenerator(email, name, id);
+        var decodedJWT = DecodeJWT(generatedJWT);
+        var UserName = decodedJWT.Claims.ElementAt(1).Value;
+        var Userid = decodedJWT.Claims.ElementAt(2).Value;
+        var Useremail = decodedJWT.Claims.ElementAt(3).Value;
+        Dictionary<string, object> expectedHeader = new Dictionary<string, object>()
+        {
+            {"alg", "HS256"},
+            {"typ", "JWT"}
+        };
+        var header = decodedJWT.Header;
+        return email == Useremail && name == UserName && id.ToString() == Userid &&
+        header["alg"].Equals(expectedHeader["alg"]) && header["typ"].Equals(expectedHeader["typ"]);
+    }
+    private JwtSecurityToken DecodeJWT(string JWT)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        return handler.ReadJwtToken(JWT);
+    }
+
+    [Test]
+    public void AdminControllerTest()
     {
         Assert.Pass();
     }
