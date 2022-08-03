c3e77d927d20a0f6ca7ab2648cbe68c57756c23c
Kristóf <bkristof54@gmail.com>
2022-08-03 19:57:30 +0200

    Changed port back

--- ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Program.cs
+++ ElProyecteGrandeSprint1/ElProyecteGrandeSprint1/Program.cs
@@ -52,7 +52,7 @@ var app = builder.Build();
 if (app.Environment.IsProduction())
 {
     var port = Environment.GetEnvironmentVariable("PORT");
-    app.Urls.Add($"http://Kecske:{port}");
+    app.Urls.Add($"http://*:{port}");
 }
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
