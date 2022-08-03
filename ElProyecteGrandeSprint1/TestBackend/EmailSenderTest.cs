176464fa5f3e43be89ceffe424ef18211fb90449
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-08-03 18:16:57 +0200

    Created test for emailsender class

---
+++ ElProyecteGrandeSprint1/TestBackend/EmailSenderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElProyecteGrandeSprint1.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TestBackend
+{
+    public class EmailSenderTest
+    {
+
+        private EmailSender _emailSender = new EmailSender();
+
+        [Test]
+        public void SendConfirmationEmailTest()
+        { 
+            _emailSender.SendConfirmationEmail("admin", "admin@admin.com", "registration", new Guid());
+            _emailSender.SendConfirmationEmail("admin", "admin@admin.com", "forgor", new Guid());
+            _emailSender.SendConfirmationEmail("admin", "admin@admin.com", "success", new Guid());
+            Assert.Pass();
+        }
+    }
+}
