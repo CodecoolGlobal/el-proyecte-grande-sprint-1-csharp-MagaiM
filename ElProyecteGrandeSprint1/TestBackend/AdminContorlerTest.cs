4391d22a216f04c873eed1fd7d4ed6e7657e38f1
Phyxick <89603303+Phyxick@users.noreply.github.com>
2022-08-03 18:16:34 +0200

    Created test for AdminControler

---
+++ ElProyecteGrandeSprint1/TestBackend/AdminContorlerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElProyecteGrandeSprint1.Controllers;
+using ElProyecteGrandeSprint1.Helpers;
+using ElProyecteGrandeSprint1.Services;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace TestBackend
+{
+    public class AdminContorlerTest : DatabaseMockInSetup
+    {
+        private AdminController _adminController;
+        private EmailSender _mockedEmailSender;
+        private UserService _service;
+        private UserServiceHelper _serviceHelper = new UserServiceHelper();
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockedEmailSender = NSubstitute.Substitute.For<EmailSender>();
+            _mockedEmailSender = mockedEmailSender;
+            _service = new UserService(_context, _serviceHelper, _mockedEmailSender);
+            _adminController = new AdminController(_service);
+        }
+
+        [Test]
+        public void TestGetAllUser()
+        {
+            Assert.AreEqual(_context.Users, _adminController.GetUsers().Result);
+        }
+    }
+}
